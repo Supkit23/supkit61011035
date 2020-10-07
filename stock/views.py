e92ffeffd50e5d1eed05c3a5cf2fe1e3208dc81f
supkit23 <suppakrit@outlook.com>
2020-10-08 03:39:57 +0700

    1st com

---
+++ stock/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+""" from django.http import HttpResponse """
+
+# Create your views here.
+""" def index(request):
+    # html = "<html><body> welcome to kk </body></html>"
+    return HttpResponse(html) """
+
+def index(request):
+    return render(request, 'frontend/index.html')
